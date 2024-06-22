d4c7cf47b64c4c64ed9e5884b0d3c42ffd45698a
3llousha <164997171+3llousha@users.noreply.github.com>
2024-06-23 01:15:14 +0200

    Update Adder8Bit.vhd

--- Adder8Bit.vhd
+++ Adder8Bit.vhd
@@ -8,7 +8,8 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity Adder8bit is
     Port ( 
         A,B : in  STD_LOGIC_VECTOR (7 downto 0); -- input A & B (8-bit)
-        Cin,Cout : in  STD_LOGIC; -- Carry in/out input
+        Cin : in  STD_LOGIC; -- Carry input
+        Cout : out  STD_LOGIC; -- Carry out
         Sum : out STD_LOGIC_VECTOR (7 downto 0);-- Sum 
     );
 end Adder8bit;
@@ -17,7 +18,8 @@ end Adder8bit;
 architecture Behavioral of Adder8bit is
     signal add_var : STD_LOGIC_VECTOR (8 downto 0); --  signal to hold the result including any carry overflow
 begin 
-    process(A, B, Cin)    -- Process for the adder, sensitive to carry in and input A/B
+    
+process(A, B, Cin)    -- Process for the adder, sensitive to carry in and input A/B
     begin
 
         add_var <= ('0' & A) + ('0' & B) + Cin;  -- Adding A, B, & Cin and store in add_var
